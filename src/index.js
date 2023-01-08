3208021c0ed15df49f24c543ded06127e86e22f7
M1chae1M <michalmaru10@gmail.com>
2023-01-08 22:58:07 +0100

    1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<BrowserRouter basename={window.location.pathname || ''}>
+<Route exact path="/" component={Index} />
+</BrowserRouter>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
